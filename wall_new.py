820550d3dd44104707bdd6f28bb6a6df6ca24ff7
Maciej Grzybacz <grzybaczm@student.agh.edu.pl>
2023-01-22 22:01:40 +0100

    Obliczenie ścian zewnętrznych

--- wall_new.py
+++ wall_new.py
@@ -4,3 +4,16 @@ import pprint as pp
 m = ifcopenshell.open("231110AC-11-Smiley-West-04-07-2007.ifc")
 walls = m.by_type("IfcWall")
 print(len(walls))
+
+
+print(f'Liczba ścian w modelu: {len(walls)}')
+
+walls = m.by_type("IfcWall")
+ext_walls = []
+for w in walls:
+    psets = ifcopenshell.util.element.get_psets(w)
+    if psets.get("Pset_WallCommon"):
+        if bool(psets.get("Pset_WallCommon").get("IsExternal")):
+            ext_walls.append(w)
+            
+print(f'Liczba ścian zewnętrznych: {len(ext_walls)}')
